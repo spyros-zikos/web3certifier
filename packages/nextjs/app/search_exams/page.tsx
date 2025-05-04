ddac10375b24fed306493a881ce544c7a0d4c52d
spyros-zikos <zikossp@gmail.com>
2025-05-04 17:04:38 +0300

    error messages fixes

--- packages/nextjs/app/search_exams/page.tsx
+++ packages/nextjs/app/search_exams/page.tsx
@@ -71,7 +71,7 @@ const SearchExamsPage: React.FC = () => {
                     onChange={(e: any) => { setShowMyExams(e.target.checked); }}
                 />
             </div>
-            {!chain?.id && <p className="text-center">Please connect your wallet on Arbitrum or Celo.</p>}
+            {!chain?.id && <p className="text-center">Connect your wallet to Arbitrum or Celo.</p>}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 space-14">
                 {(examsIdsOfPage)?.map((id, i) => (
                     <ExamCard key={i}
