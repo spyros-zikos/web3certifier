0240dd5b4a9ccfdead15f4621e0fc49614097618
spyros-zikos <zikossp@gmail.com>
2025-05-13 15:55:50 +0300

    add viewport metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata = getMetadata({
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </meta>
       <body>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
