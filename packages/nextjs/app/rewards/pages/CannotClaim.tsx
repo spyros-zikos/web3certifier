73924e4478881a555ca6b7762b92cb201bdf4e84
spyros-zikos <zikossp@gmail.com>
2025-05-16 12:31:00 +0300

    improve cannot claim message

--- packages/nextjs/app/rewards/pages/CannotClaim.tsx
+++ packages/nextjs/app/rewards/pages/CannotClaim.tsx
@@ -4,7 +4,11 @@ import PageErrorMessage from '../_components/PageErrorMessage';
 const CannotClaim = ({id}: {id: bigint}) => {
     return (
         <PageErrorMessage id={id}>
-            <div>In order to claim, you need to pass the exam!</div>
+            <div>
+                <div>Wait for the exam to end,</div>
+                <div>claim the NFT certificate</div>
+                <div>and claim this reward!</div>
+            </div>
         </PageErrorMessage>
     );
 }
