9d779e42cc42de5f2517d105539b1f71df63bcdd
spyros-zikos <zikossp@gmail.com>
2025-07-13 07:27:02 +0300

    remove voting cause it doesnt work

--- packages/nextjs/components/AnnouncementBanner.tsx
+++ packages/nextjs/components/AnnouncementBanner.tsx
@@ -1,30 +1,8 @@
-import Link from 'next/link'
 import React from 'react'
-import { useAccount } from 'wagmi';
-import { wagmiWriteToContract } from '~~/hooks/wagmi/wagmiWrite';
-
-
-
 
 const AnnouncementBanner = () => {
-    const { chain } = useAccount();
-
-  const { writeContractAsync: vote } = wagmiWriteToContract();
-  function handleVote() {
-      vote({
-          contractName: 'Council',
-          functionName: 'allocateBudget',
-          args: [
-            [
-              ["0x637365C8697C63186dC4759bd0F10af9B32D3c1A"],
-              [BigInt(10)],
-            ]
-          ],
-      });
-  }
-
   return (
-    <div className="h-8 w-full text-base-200 bg-neutral flex items-center justify-center">Support us by voting for our project <div className="underline ml-[5px]" onClick={chain?.id === 42220 ? handleVote : () => window.open("https://flowstate.network/gooddollar")}>here</div>!</div>
+    <div className="h-8 w-full text-base-200 bg-neutral flex items-center justify-center">Support us by voting for our project <div className="underline ml-[5px]" onClick={() => window.open("https://flowstate.network/gooddollar")}>here</div>!</div>
   )
 }
 
