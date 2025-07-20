f25bec4796a882723a0b427aaeb084be76408427
spyros-zikos <zikossp@gmail.com>
2025-07-20 16:32:50 +0300

    create dropdown with reward info to show on exam page

---
+++ packages/nextjs/app/exam_page/_components/RewardInfo.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { wagmiReadFromContract } from '~~/hooks/wagmi/wagmiRead';
+import { ZERO_ADDRESS } from '~~/constants';
+import { Address } from '~~/components/scaffold-eth';
+import { Accordion, Text, Spacer } from '@chakra-ui/react';
+import ExamDetail from './ExamDetail';
+
+
+const RewardInfo = ({id}: {id: bigint}) => {
+
+    /*//////////////////////////////////////////////////////////////
+                           READ FROM CONTRACT
+    //////////////////////////////////////////////////////////////*/
+
+    const rewardAddress: string  = wagmiReadFromContract({
+        contractName: "RewardFactory",
+        functionName: "getRewardByExamId",
+        args: [id],
+    }).data;
+
+    const tokenAddress: string = wagmiReadFromContract({
+        contractName: "Reward",
+        contractAddress: rewardAddress,
+        functionName: "getTokenAddress",
+    }).data;
+
+    const rewardAmountPerPerson: string = wagmiReadFromContract({
+        contractName: "Reward",
+        contractAddress: rewardAddress,
+        functionName: "getRewardAmountPerPerson",
+    }).data;
+
+    const rewardAmountPerCorrectAnswer: string = wagmiReadFromContract({
+        contractName: "Reward",
+        contractAddress: rewardAddress,
+        functionName: "getRewardAmountPerCorrectAnswer",
+    }).data;
+
+    const balance: bigint  = wagmiReadFromContract({
+        contractName: "ERC20",
+        contractAddress: tokenAddress,
+        functionName: "balanceOf",
+        args: [rewardAddress],
+    }).data;
+
+    const decimals: bigint  = wagmiReadFromContract({
+        contractName: "ERC20",
+        contractAddress: tokenAddress,
+        functionName: "decimals",
+    }).data;
+
+    const tokenName: string  = wagmiReadFromContract({
+        contractName: "ERC20",
+        contractAddress: tokenAddress,
+        functionName: "name",
+    }).data;
+
+    const tokenSymbol: string  = wagmiReadFromContract({
+        contractName: "ERC20",
+        contractAddress: tokenAddress,
+        functionName: "symbol",
+    }).data;
+    
+    if (rewardAddress === ZERO_ADDRESS)
+        return <></>;
+
+    const scaledBalance = Number(balance) / (Number(10) ** Number(decimals));
+    const scaledRewardAmountPerPerson = Number(rewardAmountPerPerson) / (Number(10) ** Number(decimals));
+    const scaledRewardAmountPerCorrectAnswer = Number(rewardAmountPerCorrectAnswer) / (Number(10) ** Number(decimals));
+
+    return (
+        <Accordion.Root borderBottom="1px solid" borderColor="lighterLighterBlack" my="0" py="2" collapsible>
+            <Accordion.Item value={"1"}>
+                <Accordion.ItemTrigger>
+                <Text fontWeight="semibold" fontSize={"lg"}>
+                    Reward Information
+                </Text>
+                <Spacer />
+                <Accordion.ItemIndicator />
+                </Accordion.ItemTrigger>
+                <Accordion.ItemContent>
+                <Accordion.ItemBody>
+                        <ExamDetail
+                            name="Available Reward Amount"
+                            value={(scaledBalance?scaledBalance.toString():"0") + " " + (tokenSymbol?tokenSymbol.toString():"unknown")}
+                        />
+                        <ExamDetail
+                            name="Reward Amount Per Person"
+                            value={(scaledRewardAmountPerPerson?scaledRewardAmountPerPerson.toString():"0") + " " + (tokenSymbol?tokenSymbol.toString():"unknown")}
+                        />
+                        <ExamDetail
+                            name="Reward Amount Per Correct Answer"
+                            value={(scaledRewardAmountPerCorrectAnswer?scaledRewardAmountPerCorrectAnswer.toString():"0") + " " + (tokenSymbol?tokenSymbol.toString():"unknown")}
+                        />
+                        <ExamDetail name="Token Name" value={tokenName?tokenName.toString():"unknown"} />
+                        <ExamDetail name="Token Symbol" value={tokenSymbol?tokenSymbol.toString():"unknown"} />
+                        <ExamDetail
+                            name="Token Address"
+                            value={tokenAddress ?
+                            <div className="inline-block">
+                                <Address address={tokenAddress} className={"text-bold"} disableAddressLink={true} />
+                            </div>
+                            : <>unknown</>}
+                        />
+                        <ExamDetail
+                            name="Reward Address"
+                            value={tokenAddress ?
+                            <div className="inline-block">
+                                <Address address={rewardAddress} className={"text-bold inline-block"} disableAddressLink={true} />
+                            </div>
+                            :<>unknown</>}
+                        />
+                    </Accordion.ItemBody>
+                </Accordion.ItemContent>
+            </Accordion.Item>
+        </Accordion.Root>
+    );
+}
+
+export default RewardInfo
