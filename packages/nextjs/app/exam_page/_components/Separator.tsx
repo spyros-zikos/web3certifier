61ea8d59ab2e2b32285e26f2aa7f88743de90122
spyros-zikos <zikossp@gmail.com>
2025-07-29 13:11:43 +0300

    simplify separator

--- packages/nextjs/app/exam_page/_components/Separator.tsx
+++ packages/nextjs/app/exam_page/_components/Separator.tsx
@@ -3,7 +3,7 @@ import {Text } from '@chakra-ui/react'
 
 const Separator = () => {
     return (
-        <Text mt="16" mb="10" borderTop="1px solid" borderColor="lighterLighterBlack"></Text>
+        <Text borderTop="1px solid" borderColor="lighterLighterBlack"></Text>
     )
 }
 
