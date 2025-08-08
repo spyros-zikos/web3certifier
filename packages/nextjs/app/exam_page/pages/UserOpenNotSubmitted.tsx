7dad5950fac23ecd499f2b0f00e4a7b014f92a5d
spyros-zikos <zikossp@gmail.com>
2025-08-08 17:29:25 +0300

    add warning for timer

--- packages/nextjs/app/exam_page/pages/UserOpenNotSubmitted.tsx
+++ packages/nextjs/app/exam_page/pages/UserOpenNotSubmitted.tsx
@@ -88,9 +88,9 @@ const UserOpenNotSubmitted = ({
             {questionNumber > 0 ?
                 <Question questionNumber={questionNumber} exam={exam} showAnswers={timeEnded ? false : true} answers={answers} setAnswers={setAnswers} />
                 : 
-                <Box display={"flex"} justifyContent={"center"} alignItems={"center"} bg="primary" w="full" h="500px" border="2px" borderStyle="solid" borderColor="lightGreen">
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"end"} alignItems={"center"} bg="primary" w="full" h="500px" border="2px" borderStyle="solid" borderColor="lightGreen">
                     <Button
-                        className="bg-base-100 w-32 h-12 font-bold text-xl"
+                        className="bg-base-100 w-32 h-12 font-bold text-xl mb-32"
                         onClick={() => {
                             const startTime = Math.floor(Date.now()/1000); 
                             Cookies.set(`w3c.${chain?.id}.${id}.${address}.startTime`, startTime.toString(), { expires: 10000 });
@@ -99,6 +99,12 @@ const UserOpenNotSubmitted = ({
                     >
                         Start
                     </Button>
+                    <Box className="flex items-center justify-center mt-8 mb-8">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                        <Box className="font-bold max-w-72 ml-4">Once you press start you will have 30 seconds to answer each question!</Box>
+                    </Box>
                 </Box>
             }
 
