7a4c7ea203f2898ee12d3115b8af6a5e6f982b34
spyros-zikos <zikossp@gmail.com>
2025-08-17 13:55:45 +0300

    hide two test exams

--- packages/nextjs/app/search_exams/page.tsx
+++ packages/nextjs/app/search_exams/page.tsx
@@ -27,7 +27,7 @@ const SearchExamsPage: React.FC = () => {
     const hidelist = {
         [SUPPORTED_NETWORKS.sepolia]: [],
         [SUPPORTED_NETWORKS.arbitrum]: [],
-        [SUPPORTED_NETWORKS.celo]: [BigInt(22), BigInt(23)]
+        [SUPPORTED_NETWORKS.celo]: [BigInt(22), BigInt(23), BigInt(26), BigInt(27)]
     };
 
     /*//////////////////////////////////////////////////////////////
