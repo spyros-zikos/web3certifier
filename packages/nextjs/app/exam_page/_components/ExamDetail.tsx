361a312e346953e4c73b2bb0298c01648a676093
spyros-zikos <zikossp@gmail.com>
2025-08-24 14:30:03 +0300

    fix to not cause hydration error

--- packages/nextjs/app/exam_page/_components/ExamDetail.tsx
+++ packages/nextjs/app/exam_page/_components/ExamDetail.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { Flex, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
 
 const ExamDetail = ({name, value}: {name: any, value: any}) => {
     return (
         <Flex>
             <label className="fontsize-12 text-accent inline-block">{name}:</label> {""}
             <Spacer />
-            <Text fontSize="12" p="0" m="0" mb="4" maxWidth={"360px"} display={"inline-block"}>{value}</Text>
+            <Box fontSize="12" p="0" m="0" mb="4" maxWidth={"360px"} display={"inline-block"}>{value}</Box>
         </Flex>
     );
 }
