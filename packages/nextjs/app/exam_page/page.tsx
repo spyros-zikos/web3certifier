0a66d3ed0a9db2de41660deb323d03b73435e281
spyros-zikos <zikossp@gmail.com>
2025-09-04 16:30:17 +0300

    show invite link message only to users connected to  Celo

--- packages/nextjs/app/exam_page/page.tsx
+++ packages/nextjs/app/exam_page/page.tsx
@@ -112,7 +112,7 @@ const ExamPage = () => {
             { address === exam?.certifier && <ManageRewardsLink id={exam?.id || BigInt(0)} /> }
 
             {/* Invite Link */}
-            { isConnected && 
+            { isConnected && chain.id === 42220 && 
             <Box fontSize="sm" mb="16" border="2px" borderStyle="solid" borderColor="lightGreen" rounded="lg" p="4">
                 <Box>Use this link to invite people and get 3k G$ tokens for every user that submits using this link!</Box>
                 <Box textDecoration="underline" color="green">https://web3certifier.com/exam_page?id=${id}&inviter=${address}</Box>
