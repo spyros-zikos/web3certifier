6a51c7e00c861ed4fff84b70df2c653ee50774d5
spyros-zikos <zikossp@gmail.com>
2025-09-19 18:59:44 +0300

    edit line

--- packages/nextjs/app/rewards/_components/SubHeading.tsx
+++ packages/nextjs/app/rewards/_components/SubHeading.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SubHeading = ({ children }: { children: React.ReactNode }) => {
-  return (
+    return (
         <div className="text-2xl font-bold mt-9">{children}</div>
     );
 }
