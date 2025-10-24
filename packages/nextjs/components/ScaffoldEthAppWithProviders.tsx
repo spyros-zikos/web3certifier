5d8878728d1bf82e097ada43f13d5bd720afc053
spyros-zikos <zikossp@gmail.com>
2025-10-24 19:08:28 +0300

    remove banner

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -21,7 +21,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <AnnouncementBanner />
+        {/* <AnnouncementBanner /> */}
         <Header />
         <main className="relative flex flex-col flex-1">{children}</main>
         {/* <Footer /> */}
