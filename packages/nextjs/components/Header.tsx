b392609fab8b9436d3f5383b0ceb6fc85c6fb38b
spyros-zikos <zikossp@gmail.com>
2025-10-29 18:12:44 +0200

    update tagline

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -113,7 +113,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Web3 Certifier</span>
-            <span className="text-xs">Exams & Certificates</span>
+            <span className="text-xs">Learn & Earn On-chain</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
