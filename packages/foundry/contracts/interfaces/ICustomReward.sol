6ab3e86956baa665e5906a32a1621621b52677a2
spyros-zikos <zikossp@gmail.com>
2025-11-23 17:25:17 +0200

    add name and description to custom reward interface and rename the getRewardAmountForUser function

--- packages/foundry/contracts/interfaces/ICustomReward.sol
+++ packages/foundry/contracts/interfaces/ICustomReward.sol
@@ -3,16 +3,28 @@ pragma solidity ^0.8.24;
 
 interface ICustomReward {
     /**
+     * @return The name of the custom reward
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @return The description of the custom reward
+     */
+    function description() external view returns (string memory);
+
+    /**
+     * @notice required if the distribution type or eligibility type is custom
      * Returns whether the user is eligible for a custom reward
      * @param user The address of the user
      */
     function isEligible(address user) external view returns (bool);
 
     /**
+     * @notice required if the distribution type is custom
      * This function returns the amount of tokens that this user should get
      * @param user The address of the user
-     * @param distributionParameter The distribution parameter
+     * @param distributionParameter The distribution parameter, has 18 decimals
      * @return The amount of tokens that this user should get
      */
-    function rewardAmount(address user, uint256 distributionParameter) external view returns (uint256);
+    function rewardAmountForUser(address user, uint256 distributionParameter) external view returns (uint256);
 }
