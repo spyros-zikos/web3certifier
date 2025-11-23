149f01fc6092292003eafc245e6d1af81e387b73
spyros-zikos <zikossp@gmail.com>
2025-11-23 17:39:29 +0200

    small changes to the reward page

--- packages/nextjs/app/rewards/page.tsx
+++ packages/nextjs/app/rewards/page.tsx
@@ -3,7 +3,7 @@
 import { wagmiReadFromContract } from "~~/hooks/wagmi/wagmiRead";
 import { useSearchParams } from "next/navigation";
 import { ZERO_ADDRESS } from "~~/constants";
-import { CreateReward, ManageReward, ClaimReward, ErrorPage } from "./pages"
+import { CreateReward, ManageReward, ErrorPage } from "./pages"
 import { useNonUndefinedAccount } from "~~/utils/NonUndefinedAccount";
 
 const Page = () => {
@@ -27,51 +27,13 @@ const Page = () => {
         args: [id],
     }).data;
 
-    const userHasClaimed = wagmiReadFromContract({
-        contractName: "Reward",
-        contractAddress: rewardAddress,
-        functionName: "getUserHasClaimed",
-        args: [address],
-    }).data;
-
-    const userHasSucceeded = wagmiReadFromContract({
-        contractName: "Reward",
-        contractAddress: rewardAddress,
-        functionName: "getUserHasSucceeded",
-        args: [address],
-    }).data;
-
-    const rewardAmount: bigint  = wagmiReadFromContract({
-        contractName: "Reward",
-        contractAddress: rewardAddress,
-        functionName: "getRewardAmountForUser",
-        args: [address],
-    }).data;
-
-    const totalRewardAmount: bigint  = wagmiReadFromContract({
-        contractName: "Reward",
-        contractAddress: rewardAddress,
-        functionName: "getBalance",
-    }).data;
-
     if (exam?.certifier === address)
         if (rewardAddress === ZERO_ADDRESS)
             return <CreateReward id={id} />
         else
             return <ManageReward id={id} />
     else
-        if (rewardAddress === ZERO_ADDRESS)
-            return <ErrorPage id={id} chain={chain} message="This reward does not exist!" />
-        else if (!userHasClaimed && userHasSucceeded && rewardAmount !== BigInt(0) && totalRewardAmount >= rewardAmount)
-            return <ClaimReward id={id} />
-        else if (!userHasClaimed && userHasSucceeded && rewardAmount === BigInt(0))
-            return <ErrorPage id={id} chain={chain} message="Your reward is zero. Either the certifier has not set a reward amount or you dont qualify for this reward." />
-        else if (userHasClaimed)
-            return <ErrorPage id={id} chain={chain} message="You already claimed this reward!" />
-        else if (!userHasSucceeded)
-            return <ErrorPage id={id} chain={chain} message="Wait for the exam to end, claim the NFT certificate and claim this reward!" />
-        else if (totalRewardAmount < rewardAmount)
-            return <ErrorPage id={id} chain={chain} message="The reward pool does not have enough tokens to reward you." />
+        return <ErrorPage id={id} chain={chain} message="You are not the organizer!" />
 }
 
 export default Page;
