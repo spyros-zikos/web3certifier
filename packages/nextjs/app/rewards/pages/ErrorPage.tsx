c7d0b043a8880fb0db4bf3af8a9523b26f3ea3ba
spyros-zikos <zikossp@gmail.com>
2025-11-23 17:34:56 +0200

    make the error page simpler

--- packages/nextjs/app/rewards/pages/ErrorPage.tsx
+++ packages/nextjs/app/rewards/pages/ErrorPage.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { PageWrapper } from '~~/components'
-import TitleWithLinkToExamPage from '../_components/TitleWithLinkToExamPage'
-import RewardInfo from '../_components/RewardInfo'
+import { TitleWithLinkToExamPage } from '../components'
 
 const ErrorPage = ({ message, chain, id }: { message: string, chain: any, id: bigint }) => {
   return (
     <PageWrapper>
-        <TitleWithLinkToExamPage id={id}>Claim Reward</TitleWithLinkToExamPage>
+        <TitleWithLinkToExamPage id={id}>Error</TitleWithLinkToExamPage>
         <div className="text-3xl font-bold my-4 mx-auto text-base-100">
             {message}
         </div>
-        <RewardInfo id={id} chain={chain}/>
     </PageWrapper>
   )
 }
