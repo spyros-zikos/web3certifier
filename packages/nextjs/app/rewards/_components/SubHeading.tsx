4589a591d45b175de86fdd5e0b46c8b30f58b718
spyros-zikos <zikossp@gmail.com>
2025-11-23 17:33:32 +0200

    rename components directory and remove some components that are not used

--- packages/nextjs/app/rewards/_components/SubHeading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SubHeading = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="text-2xl font-bold mt-9">{children}</div>
-    );
-}
-
-export default SubHeading
